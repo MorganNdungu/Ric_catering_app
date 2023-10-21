297bcba7adf9d479f4f71fb0008a256ef121b194
MorganNdungu <ndungumorgan20@gmail.com>
2023-10-21 14:31:40 +0300

    footer css

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Add your CSS styles here -->
+    <link rel="stylesheet" type="text/css" href="your-styles.css">
+</head>
+<body>
+    <!-- Your website content -->
+
+    <!-- Footer -->
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h4>About Us</h4>
+                    <p>Your catering app provides delicious food for all occasions. We offer a wide range of services to make your events special.</p>
+                </div>
+                <div class="col-md-4">
+                    <h4>Follow Us</h4>
+                    <ul class="social-icons">
+                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fab fa-whatsapp"></i></a></li>
+                        <li><a href="#"><i class="fab fa-tiktok"></i></a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h4>Contact Us</h4>
+                    <p>Phone: +123-456-789</p>
+                    <p>Email: info@cateringapp.com</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="copyright">Site designed by Morgan Ndung'u &copy; {{ date('Y') }}</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
